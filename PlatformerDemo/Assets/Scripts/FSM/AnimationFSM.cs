4ce7b9e8a9349acf4d24ce51f14c7f40ff68a102
GreatJoey <ej95050@gmail.com>
2025-09-16 10:36:50 -0700

    Added stuff

---
+++ PlatformerDemo/Assets/Scripts/FSM/AnimationFSM.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AnimationFSM
+{
+    public struct Conditions
+    {
+        public bool isOnGround;
+        public bool movingX;
+    }
+
+    public abstract class State
+    {
+        public string animationName;
+
+        protected State(string animationName)
+        {
+            this.animationName = animationName;
+        }
+
+        public abstract bool IsMatchingConditions(Conditions conditions);
+    }
+
+    public class FSM
+    {
+        public Conditions conditions;
+        public State currentState;
+        List<State> states = new List<State>();
+
+        public void AddState(State state)
+        {
+            states.Add(state);
+        }
+        public void Update()
+        {
+            foreach (var state in states)
+            {
+                if (state.IsMatchingConditions(conditions))
+                {
+                    currentState = state;
+                    break;
+                }
+            }
+        }
+    }
+}
--- PlatformerDemo/Assets/Scripts/PlayerController.cs
+++ PlatformerDemo/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,6 +13,15 @@ public class PlayerController : MonoBehaviour
     InputAction MoveRight;
     InputAction MoveLeft;
     InputAction Jump;
+
+    SpriteRenderer sprite;
+
+    [SerializeField] private Animator _animator;
+
+    AnimationFSM.FSM fsm = new AnimationFSM.FSM();
+
+    private Vector2 lastFacingDir = Vector2.right;
+    private Vector2 FacingDir => lastFacingDir;
 
     float XVelocity;
     float YVelocity;
@@ -26,11 +38,44 @@ public class PlayerController : MonoBehaviour
         Jump = InputSystem.actions.FindAction("Jump");
 
         RigidBody = GetComponent<Rigidbody2D>();
+        //Assert.NotNull(RigidBody);
+
         GroundCollider = GetComponent<CapsuleCollider2D>();
+        //Assert.NotNull(GroundCollider);
+
+        _animator = GetComponent<Animator>();
+        //Assert.NotNull(_animator);
+        fsm.AddState(new AnimationFSM.JumpState("Jumping"));
+        fsm.AddState(new AnimationFSM.WalkState("Walking"));
+        fsm.AddState(new AnimationFSM.IdleState("Idle"));
+
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        OnGround = GroundCollider.IsTouchingLayers(LayerMask.GetMask("Enviroment"));
+
+        fsm.Update();
+
+        var animatorState = _animator.GetCurrentAnimatorStateInfo(0);
+        fsm.conditions.isOnGround = OnGround;
+        fsm.conditions.movingX = (XVelocity != 0);
+
+        var fsmAnimationName = fsm.currentState.animationName;
+        if (!animatorState.IsName(fsmAnimationName))
+        {
+            _animator.Play(fsmAnimationName);
+        }
+
+        if (XVelocity > 0)
+        {
+            sprite.flipX = false;
+        }
+        else if (XVelocity < 0)
+        {
+            sprite.flipX = true;
+        }
         GetInput();
     }
 
